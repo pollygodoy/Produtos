470353ccc778495f364ec16558f13465a70c6698
Poliane F De Godoy <122944387+pollygodoy@users.noreply.github.com>
2023-03-23 22:06:14 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,16 @@
+public class Produto {
+    String nome;
+    Double preco;
+    Integer quantidae;
+
+    Double totalValorEmEstoque() {
+        return 0.0;
+
+    }
+    void addProdutos() {
+
+    }
+    void removeProdutos(){
+
+    }
+}
